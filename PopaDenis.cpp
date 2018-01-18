552fb3ba64ce3d8e63ce3c38798150d2c3081c38
popadenys <35447008+popadenys@users.noreply.github.com>
2018-01-18 07:26:56 +0200

    Update PopaDenis.cpp

--- PopaDenis.cpp
+++ PopaDenis.cpp
@@ -1,78 +1,44 @@
-/*Popa Denis
-Nr2. Simple cozi*/
-
 #include<iostream>
+#include<conio.h>
 using namespace std;
 
 struct nod{
   int nr;
   nod* leg;
 };
+nod* v,*sf;
+int n;
 
-nod* v,*sf,*d,*c;
-int val,val1,i,info,n;
-
-int adaugare(nod*& v,nod*& sf,int val){
+int Pune(nod*& v,nod*& sf,int n){
   nod* c;
-  if(v==0){
-    v=new(nod);
-    v->nr=val;
+  if (!v){
+    v=new nod;
+    v->nr=n;
     v->leg=0;
     sf=v;
   } else {
-    c=new(nod);
+    c=new nod;
     sf->leg=c;
-    c->nr=val;
+    c->nr=n;
     c->leg=0;
     sf=c;
   }
-};
-
-int inserare_dupa(nod* v,nod*&sf,int val,int val1){
-  nod* c=v,*d;
-  while(c->nr!=val) c=c->leg;
-  d=new nod;
-  d->nr=val1;
-  d->leg=c->leg;
-  c->leg=d;
-  if (d->leg==0) sf=d;
-};
-
-int inserare_inainte(nod*&v,int val,int val1){
-  nod*c ,*d;
-  if (v->nr==val){
-    d=new nod;
-    d->nr=val1;
-    d->leg=v;
-    v=d;
-  } else {
-    c=v;
-    while (c->leg->nr!=val) c=c->leg;
-    	d=new nod;
-    d->nr=val1;
-    d->leg=c->leg;
-    c->leg=d;
-  }
 }
 
-int sterge(nod*& v,nod*& sf,int val){
-  nod* c,*man;
-  if (v->nr==val){
-    man=v;
-    v=v->leg;
-  } else {
+int Scoate(nod*& v){
+  nod* c;
+  if (!v) cout<<"Coada este vida!";
+  else {
+    cout<<"Am scos "<<v->nr<<endl;
     c=v;
-    while(c->leg->nr!=val) c=c->leg;
-    man=c->leg;
-    c->leg=man->leg;
-    if (man==sf) sf=c;
+    v=v->leg;
+    delete c;
   }
-  delete man;
 }
 
 int listare(nod* v){
-  nod *c=v;
-  while(c){
+  nod* c=v;
+  while (c){
     cout<<c->nr<<" ";
     c=c->leg;
   }
@@ -80,22 +46,12 @@ int listare(nod* v){
 }
 
 int main(int argc,char* argv[]){
-  cout<<"Dati numarul de noduri:";cin>>n;
-  for (i=1;i<=n;i++) {
-    cout<<"info:";cin>>info;
-    adaugare(v,sf,info);
-  }
-  listare(v);
-  cout<<"inserati valoarea:";cin>>val1;
-  cout<<"dupa nodul cu informatia:";cin>>val;
-  inserare_dupa(v,sf,val,val1);
-  listare(v);
-  cout<<"inserati valoarea:";cin>>val1;
-  cout<<"inaintea nodului cu informatia:";cin>>val;
-  inserare_inainte(v,val,val1);
-  listare(v);
-  cout<<"stergeti nodul de informatie:";cin>>val;
-  sterge(v,sf,val);
-  listare(v);
+  Pune(v,sf,1);Pune(v,sf,2);
+  Pune(v,sf,3);listare(v);
+  Scoate(v);listare(v);
+  Scoate(v);listare(v);
+  Scoate(v);
+  Scoate(v);
+
   return 0;
 }
