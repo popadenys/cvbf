a7b8dd303224d02c5bf8f4728725045f7e17c4de
popadenys <35447008+popadenys@users.noreply.github.com>
2018-01-19 10:52:47 +0200

    Update PopaDenis.cpp

--- PopaDenis.cpp
+++ PopaDenis.cpp
@@ -1,56 +1,96 @@
 #include<iostream>
 using namespace std;
 
-struct nod{
-  int nr;
-  nod* leg;
-};
-nod* v,*sf;
-int n;
+typedef struct termen {
+                struct termen *next;
+                int info;
+                } element;
 
-int Pune(nod*& v,nod*& sf,int n){
-  nod* c;
-  if (!v){
-    v=new nod;
-    v->nr=n;
-    v->leg=0;
-    sf=v;
-  } else {
-    c=new nod;
-    sf->leg=c;
-    c->nr=n;
-    c->leg=0;
-    sf=c;
-  }
+typedef struct
+              {
+               element *start,*current,*finalist;
+               int length;
+              } lista;
+
+lista *L;
+
+int show_me(lista *L) {
+    if (L->length != 0) {
+        element *carrier;
+        carrier=L->start;
+        while (carrier!=NULL) {
+            cout<<carrier->info<<" ";
+            carrier=carrier->next;
+            }
+        cout<<endl;
+        return 1;
+    } else {
+        cout << "Empty" << endl;
+    }
+    return 0;
 }
 
-int Scoate(nod*& v){
-  nod* c;
-  if (!v) cout<<"Coada este vida!";
-  else {
-    cout<<"Am scos "<<v->nr<<endl;
-    c=v;
-    v=v->leg;
-    delete c;
-  }
+int add(lista **L,int value)  {
+    if ((*L)->length == 0)  {
+        element *new_one=new element;
+        if (new_one!=NULL){
+            new_one->info=value;
+            new_one->next=NULL;
+            (*L)->length=1;
+            (*L)->start=(*L)->current=(*L)->finalist=new_one;
+        }
+        return 1;
+    }
+    element *new_one=new element;
+    if (new_one!=NULL) {
+        new_one->info=value;
+        new_one->next=NULL;
+        (*L)->length++;
+        (*L)->finalist->next=new_one;
+        (*L)->finalist=new_one;
+    }
+    return 1;
 }
 
-int listare(nod* v){
-  nod* c=v;
-  while (c){
-    cout<<c->nr<<" ";
-    c=c->leg;
-  }
-  cout<<endl;
+int serve(lista **L,int *old) {
+    if((*L)->length == 0) {
+        return 0;
+    } else if ((*L)->length == 1) {
+        (*old) = (*L)->start->info;
+        (*L)->length = 0;
+        delete (*L)->current;
+        (*L)->current=(*L)->finalist=(*L)->start=NULL;
+        return 1;
+    } else {
+        (*old) = (*L)->start->info;
+        (*L)->length--;
+        element * temp = (*L)->start;
+        if ((*L)->current == (*L)->start) {
+            (*L)->current = (*L)->start->next;
+        }
+        (*L)->start = (*L)->start->next;
+        delete temp;
+        return 1;
+    }
 }
 
-int main(int argc,char* argv[]){
-  Pune(v,sf,1);Pune(v,sf,2);
-  Pune(v,sf,3);listare(v);
-  Scoate(v);listare(v);
-  Scoate(v);listare(v);
-  Scoate(v);
-  Scoate(v);
+lista *initlist(){
+    lista *result=new lista;
+    if (result!=NULL) {
+        result->length=0;
+        result->start=result->current=result->finalist=NULL;
+    }
+    return result;
+}
 
-  return 0;
+int main() {
+    L=initlist();
+    for(int i = 1; i <= 10; i++) add(&L,i);
+    show_me(L);
+    for(int i = 0; i < 10; i++) {
+        int old;
+        serve(&L, &old);
+        cout << "Removed " << old << endl;
+        show_me(L);
+    }
 }
